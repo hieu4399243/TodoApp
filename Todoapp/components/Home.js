fe60d26f2253660faad6a18ece5bbf4793a77f8a
hieu4399243 <hieu243268@gmail.com>
2023-08-17 00:31:29 +0700

    components

---
+++ Todoapp/components/Home.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Header from './Header'
+const Home = () => {
+  return (
+    <Header/>
+  )
+}
+
+export default Home
